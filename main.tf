51c730183a102bd5b42f4eb4bff518b8c5e6bb23
getselvamk <109408167+getselvamk@users.noreply.github.com>
2025-02-13 13:24:19 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,14 @@
 provider "aws" {
-    region = "us-east-1"  
+  region = "us-east-1"
 }
 
 resource "aws_instance" "foo" {
-  ami           = "ami-05fa00d4c63e32376" # us-west-2
+  ami           = "ami-05fa00d4c63e32376" # Ensure this AMI is available in us-east-1
   instance_type = "t2.micro"
+  subnet_id     = "subnet-0e9023d44a1f96322" # Subnet in the specified VPC
+  vpc_security_group_ids = ["sg-092816b294b0010e2"] # Use the existing security group
+
   tags = {
-      Name = "TF-Instance"
+    Name = "TF-Instance"
   }
 }
